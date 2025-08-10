8c7397f90424f89f049e1f0a67ce33fda0e23237
Arthur Fernandes <arthur0139@gmail.com>
2025-08-10 16:48:51 -0300

    first commit

---
+++ src/app/root/root.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent
+  ],
+  templateUrl: './root.component.html',
+  styleUrl: './root.component.scss'
+})
+export class RootComponent {
+
+}
